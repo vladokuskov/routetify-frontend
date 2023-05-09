cfd190a62cf4c2348aa3658ffa360b0c11f18e49
Swappnet <advance11ua@gmail.com>
2023-05-09 18:11:07 +0300

    added site config and favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { siteConfig } from '@/config/site';
 import StyledComponentsRegistry from '@/lib/registry';
 import { Providers } from '@/redux/provider';
 import { Roboto } from 'next/font/google';
@@ -8,9 +9,34 @@ const roboto = Roboto({
 });
 
 export const metadata = {
-  title: 'cycroute',
-  description:
-    'Web application to create bike routes and export them in GPX/KML format. ',
+  title: siteConfig.name,
+  description: siteConfig.description,
+  keywords: ['cycroute', 'Bike', 'Route', 'GPX', 'KML'],
+  authors: [
+    {
+      name: 'swappnet',
+      url: 'https://vladokuskov.xyz/',
+    },
+  ],
+  creator: 'swappnet',
+  themeColor: [
+    { media: '(prefers-color-scheme: light)', color: 'white' },
+    { media: '(prefers-color-scheme: dark)', color: 'black' },
+  ],
+  openGraph: {
+    type: 'website',
+    locale: 'en_US',
+    url: siteConfig.url,
+    title: siteConfig.name,
+    description: siteConfig.description,
+    siteName: siteConfig.name,
+  },
+  icons: {
+    icon: '/favicon.ico',
+    shortcut: '/favicon-16x16.png',
+    apple: '/apple-touch-icon.png',
+  },
+  manifest: `${siteConfig.url}/site.webmanifest`,
 };
 
 export default function RootLayout({
---
+++ config/site.ts
@@ -0,0 +1,11 @@
+import { SiteConfig } from '@/types/global/siteConfig.types';
+
+export const siteConfig: SiteConfig = {
+  name: 'cycroute',
+  description:
+    'Web application to create bike routes and export them in GPX/KML format.',
+  url: 'https://cycroute.vercel.app',
+  links: {
+    github: 'https://github.com/swappnet/cycroute',
+  },
+};

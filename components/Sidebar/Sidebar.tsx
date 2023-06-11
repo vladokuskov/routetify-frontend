aac7d0abbbf87fbba4091ca3252feb5795fc9ee8
Swappnet <advance11ua@gmail.com>
2023-06-11 15:49:43 +0300

    cleaned

--- components/Sidebar/Sidebar.tsx
+++ components/Sidebar/Sidebar.tsx
@@ -8,7 +8,6 @@ import { SidebarSection } from './SidebarSection'
 import { Details } from './SidebarSections/Details/Details'
 import { Export } from './SidebarSections/Export/Export'
 import { Geocoder } from './SidebarSections/Geocoder/Geocoder'
-import { ThemeSwitcher } from './SidebarSections/ThemeSwitcher/ThemeSwitcher'
 
 const Sidebar = () => {
   const isSidebarOpen = useAppSelector(
@@ -26,14 +25,12 @@ const Sidebar = () => {
         'absolute overflow-visible max-w-sidebar min-w-sidebarSmall right-0 top-0 mt-0 h-[100lvh] bg-app z-20   transition-transform border-l-2 border-sidebar ',
         isSidebarOpen ? 'w-sidebar p-4' : 'w-sidebarSmall p-1',
         'max-sm:relative max-sm:p-4 max-sm:w-full max-sm:min-w-full max-sm:max-w-full max-sm:mt-[70vh] max-sm:shadow-md max-sm:shadow-black max-sm:border-t-2 max-sm:border-l-0',
-        ' dark:bg-neutral-800',
       )}
     >
       <button
         className={clsx(
           'absolute p-2 rounded-full left-[-1.25rem] top-1/2 bg-neutral-10 border-2 border-sidebar z-30 bg-neutral-100 text-neutral-400',
           'transition-colors hocus:bg-neutral-200 hocus:text-neutral-500',
-          'dark:bg-neutral-600 dark:text-neutral-400 dark:hocus:bg-neutral-500 dark:hocus:text-neutral-300',
           'max-sm:hidden',
         )}
         onClick={handleSidebarResize}
@@ -73,7 +70,6 @@ const Sidebar = () => {
           <Export />
         </SidebarSection>
         <div className="flex flex-col items-center justify-center gap-5 mt-auto mb-4">
-          <ThemeSwitcher />
           <a
             href="https://github.com/swappnet/routetify"
             rel="noopener noreferrer"

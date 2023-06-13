49b0551daa1de2e22042a405c53d1976d6ce73a0
Swappnet <advance11ua@gmail.com>
2023-06-13 20:02:08 +0300

    added uploading for gpx and kml files

--- components/Sidebar/SidebarSections/RouteUploading/RouteUploading.tsx
+++ components/Sidebar/SidebarSections/RouteUploading/RouteUploading.tsx
@@ -1,51 +1,78 @@
 import Icon from '@/components/Icon/Icon'
+import fitBounds from '@/lib/fitBounds'
+import { parseFile } from '@/lib/routeFileParses'
+import {
+  getFileExtension,
+  isGPXFileType,
+  isKMLFileType,
+  validateFileStructure,
+} from '@/lib/validations/routeFileValidation'
+import { putDrawCoords } from '@/redux/features/drawSlice'
 import { useAppDispatch, useAppSelector } from '@/redux/hooks'
 import clsx from 'clsx'
-import { ChangeEvent, useEffect, useRef, useState } from 'react'
+import { ChangeEvent, useRef } from 'react'
 import FileImportIcon from '../../../../assets/icons/file-import.svg'
+
+//TODO fix KML file uploading
 
 const RouteUploading = () => {
   const inputRef = useRef<HTMLInputElement>(null)
-  const [file, setFile] = useState<File | null>(null)
-  const drawCoords = useAppSelector((state) => state.drawReducer.drawCoords)
+
+  const map = useAppSelector((state) => state.controlsReducer.map)
   const isSidebarOpen = useAppSelector(
     (state) => state.controlsReducer.isSidebarOpen,
   )
+
   const dispatch = useAppDispatch()
 
-  const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
-    if (e.target.files) {
-      if (drawCoords.length > 0) {
-        const isConfirmed = window.confirm(
-          'Your current route will be replaced, are you sure?',
-        )
+  const handleFileChange = async (e: ChangeEvent<HTMLInputElement>) => {
+    if (!e.target.files) return
 
-        if (isConfirmed) {
-          setFile(e.target.files[0])
+    const selectedFile = e.target.files[0]
+    const fileName = selectedFile.name
+
+    const isGPX = await isGPXFileType(fileName)
+    const isKML = await isKMLFileType(fileName)
+
+    if (isGPX || isKML) {
+      const isFileStructureValid = await validateFileStructure(selectedFile)
+
+      if (isFileStructureValid) {
+        const extension = await getFileExtension(fileName)
+
+        if (extension) {
+          const route = await parseFile(selectedFile, extension)
+
+          dispatch(putDrawCoords(route))
+
+          fitBounds(map, route)
+        } else {
+          console.error('Choose another file, this file is incompatible.')
         }
+      } else {
+        console.error('Choose another file, this file is incompatible.')
       }
-
-      setFile(e.target.files[0])
+    } else {
+      console.error('Select the correct file type!')
     }
+
+    // Reset selected file
+    e.target.value = ''
   }
-
-  useEffect(() => {
-    if (file) {
-      const displayRoute = async () => {
-        if (file.type === 'gpx') {
-        } else if (file.type === 'kml') {
-        }
-      }
-
-      displayRoute()
-    }
-  }, [file])
 
   return (
     <>
       <button
         className="inline-flex justify-center items-center gap-2 w-full p-2 bg-neutral-300 rounded-md font-sans font-semibold text-neutral-700 hocus:bg-neutral-200 hocus:text-neutral-500 transition-colors"
-        onClick={() => inputRef.current?.click()}
+        onClick={() => {
+          const isConfirmed = window.confirm(
+            'If you have route, it will be replaced, continue?',
+          )
+
+          if (!isConfirmed) return
+
+          inputRef.current?.click()
+        }}
         title="Choose a file"
       >
         <span>

8400b0160d6622a2af99c5fb3db8df2b84ea9d0d
Vladyslav Okuskov <111042761+swappnet@users.noreply.github.com>
2023-09-09 13:14:47 +0300

    Update ResizeSidebarButton.tsx

--- components/Sidebar/ResizeSidebarButton/ResizeSidebarButton.tsx
+++ components/Sidebar/ResizeSidebarButton/ResizeSidebarButton.tsx
@@ -19,7 +19,7 @@ const ResizeSidebarButton = () => {
   return (
     <button
       className={clsx(
-        'absolute py-2 w-7 rounded-l-md -left-7 top-1/2 border-2 border-sidebar z-30 bg-neutral-100 text-neutral-400 !bg-opacity-90',
+        'absolute py-2 w-7 rounded-l-md -left-7 top-1/2 border-2 border-sidebar z-30 bg-neutral-100 text-neutral-400 !bg-opacity-90 active:scale-90',
         'transition-colors hocus:text-neutral-500 hocus:!border-neutral-400',
         'dark:bg-neutral-700 dark:text-neutral-300 dark:hocus:border-neutral-500 dark:active:!border-neutral-300 dark:border-neutral-600 dark:hocus:text-neutral-200',
         'max-sm:hidden max-dsm:bottom-12 max-dsm:top-auto',

4c5cbef26591390cc724ddf55abc9d1fc875185f
Swappnet <advance11ua@gmail.com>
2023-09-13 21:33:59 +0300

    updated middleware for file verifying

--- server/build/src/components/route/route.service.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.parse = void 0;
-const parse = async (file, extension) => {
-    return [];
-};
-exports.parse = parse;
-//# sourceMappingURL=route.service.js.map

91d85ebade6419778908e2fb3de57f7c03496782
Swappnet <advance11ua@gmail.com>
2023-09-17 09:23:07 +0300

    fixes

--- server/src/core/utils/verifyFileStructure.ts
+++ server/src/core/utils/verifyFileStructure.ts
@@ -6,25 +6,31 @@ const verifyFileStructure = async (
   file: Express.Multer.File,
   extension: Extension,
 ) => {
-  const fileContent = file.buffer.toString()
+  try {
+    const fileContent = file.buffer.toString()
 
-  if (fileContent.length === 0) {
-    throw new Error('Provide a file with content inside.')
-  }
+    if (fileContent.length === 0) {
+      throw new Error('Provide a file with content inside.')
+    }
 
-  const doc = new DOMParser().parseFromString(fileContent, 'text/xml')
+    const doc = new DOMParser().parseFromString(fileContent, 'text/xml')
 
-  let converted
-  if (extension === 'gpx') {
-    converted = toGeoJSON.gpx(doc)
-  } else if (extension === 'kml') {
-    converted = toGeoJSON.kml(doc)
-  }
+    let converted
+    if (extension === 'gpx') {
+      converted = toGeoJSON.gpx(doc)
+    } else if (extension === 'kml') {
+      converted = toGeoJSON.kml(doc)
+    }
 
-  if (!hasCoordinates(converted)) {
-    throw new Error(
-      `Provide a ${extension.toUpperCase()} file with coordinates inside or check structure.`,
-    )
+    if (!hasCoordinates(converted)) {
+      throw new Error(
+        `Provide a ${extension.toUpperCase()} file with coordinates inside or check structure.`,
+      )
+    }
+  } catch (error) {
+    if (error instanceof Error) {
+      throw new Error(error.message)
+    }
   }
 }
 

d7778abe8f499af0b7c20a6bcf84ed8adf8a3729
Swappnet <advance11ua@gmail.com>
2023-09-25 14:04:16 +0300

    added loader for page

--- client/app/auth/layout.tsx
+++ client/app/auth/layout.tsx
@@ -1,33 +1,39 @@
-'use client';
+'use client'
 
-import { getUser } from '@/lib/api/user';
-import { useRouter } from 'next/navigation';
-import { useEffect, useState } from 'react';
+import { getUser } from '@/lib/api/user'
+import { useRouter } from 'next/navigation'
+import { useEffect, useState } from 'react'
 
 export default function AuthLayout({
   children,
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode
 }) {
-  const router = useRouter();
-  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const router = useRouter()
+  const [isLoading, setIsLoading] = useState<boolean>(true)
 
   const getUserStatus = async () => {
-    const isLogged = await getUser();
+    const isLogged = await getUser()
 
     if (isLogged) {
-      router.push('/');
+      router.push('/')
     } else {
-      setIsLoading(false);
+      setIsLoading(false)
     }
-  };
+  }
   useEffect(() => {
-    getUserStatus();
-  }, []);
+    getUserStatus()
+  }, [])
 
   if (isLoading) {
-    return <p>Loading ..</p>;
+    return (
+      <div className="flex items-center justify-center w-full mt-4">
+        <div className="px-3 py-1 text-xs font-medium text-center text-muted-foreground rounded-full animate-pulse bg-muted">
+          loading...
+        </div>
+      </div>
+    )
   }
 
-  return <main>{children}</main>;
+  return <main>{children}</main>
 }
